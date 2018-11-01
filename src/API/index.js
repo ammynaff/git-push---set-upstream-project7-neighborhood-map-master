0569c8feabee04a1772fdd0e950612e98a9ace16
Amy Naff <ammynaff@charter.net>
2018-11-01 16:02:32 -0700

    Updating code

--- src/API/index.js
+++ src/API/index.js
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 class Helper {
     static baseURL() {
         return "https://api.foursquare.com/v2"
@@ -12,11 +14,40 @@ class Helper {
     .displaymap(key => `${key}=${keys[key]}`)
     .join("&");
 }
+static urlBuilder(urlPrams){
+    if(!urlPrams){
+        return "";
+    }
+    return Object.keys(urlPrams)
+    .displaymap(key => `${key}=${urlPrams[key]}`)
+    .join("&");
+}
 static headers(){
     return {
         Accept: "application/json"
     };
 }
-static simpleFetch(endPoint,method,urlparams) {
-    
+static simpleFetch(endPoint,method,urlPrams) {
+    let requestData = {
+        method, 
+        header: Helper.header()
+    };
+    return fetch(
+        `${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+            urlPrams
+            )}`,
+    requestData
+    ).then(res => res.json());
+}
+}
+export default class SquareAPI {
+    static search(urlPrams){
+        return Helper.simpleFetch("/venues/search", "GET", urlPrams);
+    }
+    static getVenueDetails(VENUE_ID){
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET")
+    }
+    static getVenuePhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+    }
 }
--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 import './App.css';
 import DisplayMap from './components/DisplayMap.js';
+import SquareAPI from "./API"
 
 class App extends Component {
+
+  componentDidMount(){
+    SquareAPI.search({
+      query: "winery",
+      intent:"browse",
+      radius:"50000",
+      near: "Cambria", 
+      limit: 10  
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
